bed0a4e4ad9b6ab607384f6445407cf27b621045
Ryan Birmingham <birm@rbirm.us>
2020-01-29 09:57:42 -0500

    cleaner autoinsert

--- repo/setup.js
+++ repo/setup.js
@@ -1,14 +1,19 @@
 function setupData(targetVersion){
   var db = new Dexie("cptac_repo_view");
   db.version(1).stores({repo: "id++,project,modality,site"})
-  if (window.localStorage.getItem("cptac_repo_version") !== targetVersion){
-    // add new data
-    data = fetch("data.json").then(x=>x.json())
-    data.then(x=>{
-      db.repo.bulkPut(x).then(x=>{
-        window.localStorage.setItem("cptac_repo_version", targetVersion)
+  if (window.localStorage.getItem("cptac_repo_version") < targetVersion){
+    // clear old data
+    db.repo.clear().then(x=>{
+      console.log(x)
+      // add new data
+      data = fetch("data.json").then(x=>x.json())
+      data.then(x=>{
+        db.repo.bulkPut(x).then(x=>{
+          window.localStorage.setItem("cptac_repo_version", targetVersion)
+          // ok, now we can refresh
+          window.location.reload()
+        })
       })
-      // set version to target
     })
   } else {
     console.log("already have this version of data")
