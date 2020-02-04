67f47fe07b722b86514113bcb903ace39bba4332
Ryan Birmingham <birm@rbirm.us>
2020-02-04 16:16:34 -0500

    event fire on chart click

--- repo/visLifecycle.js
+++ repo/visLifecycle.js
@@ -1,14 +1,24 @@
-
-function supportClick(chart){
+function supportClick(chart, type) {
   chart.canvas.onclick = function(evt) {
-       var activePoints = chart.getElementsAtEvent(evt);
-       // let's say you wanted to perform different actions based on label selected
-       console.log(chart.config.data.labels[activePoints[0]._index])
+    var activePoints = chart.getElementsAtEvent(evt);
+    if (activePoints && activePoints.length > 0) {
+      //console.log(chart.config.data.labels[activePoints[0]._index], type)
+      let event = new CustomEvent('visClick', {
+        detail: {
+          type: type,
+          label: chart.config.data.labels[activePoints[0]._index]
+        }
+      });
+      document.dispatchEvent(event);
+    }
   }
 }
+// TODO make this update "s" dynamically
+document.addEventListener('visClick', x => {
+  console.log(x.detail)
+})
 
-
-function draw_vis(db_object){
+function draw_vis(db_object) {
   // make the table
   db_object.toArray(makeTable)
   db_object.toArray(x => {
@@ -81,50 +91,50 @@ function draw_vis(db_object){
         }]
       }
     })
-    supportClick(CHARTS.pieChart)
-    supportClick(CHARTS.barChart)
+    supportClick(CHARTS.pieChart, "modality")
+    supportClick(CHARTS.barChart, "site")
   })
 }
 
-function replaceChartData(chart, newData, newLabel){
+function replaceChartData(chart, newData, newLabel) {
   chart.data.labels = [];
-  chart.data.labels= newLabel;
+  chart.data.labels = newLabel;
   chart.data.datasets.forEach((dataset) => {
     dataset.data = [];
-    dataset.data= newData;
+    dataset.data = newData;
   });
   chart.update();
 }
 
 
-function redraw_vis(db_object){
-    // make the table
-    db_object.toArray(makeTable)
-    db_object.toArray(x => {
-      // convert to sums by site
-      let siteCounts = {}
-      let modeCounts = {}
-      let bubbles = []
-      for (let j = 0; j < x.length; j++) {
-        if (siteCounts.hasOwnProperty(x[j].site)) {
-          siteCounts[x[j].site] = siteCounts[x[j].site] + x[j].imageCount
-        } else {
-          siteCounts[x[j].site] = x[j].imageCount
-        }
-        if (modeCounts.hasOwnProperty(x[j].modality)) {
-          modeCounts[x[j].modality] = modeCounts[x[j].modality] + x[j].imageCount
-        } else {
-          modeCounts[x[j].modality] = x[j].imageCount
-        }
-        bubbles.push({
-          x: x[j].subjectCount,
-          y: x[j].studyCount,
-          r: Math.log(x[j].imageCount)
-        })
+function redraw_vis(db_object) {
+  // make the table
+  db_object.toArray(makeTable)
+  db_object.toArray(x => {
+    // convert to sums by site
+    let siteCounts = {}
+    let modeCounts = {}
+    let bubbles = []
+    for (let j = 0; j < x.length; j++) {
+      if (siteCounts.hasOwnProperty(x[j].site)) {
+        siteCounts[x[j].site] = siteCounts[x[j].site] + x[j].imageCount
+      } else {
+        siteCounts[x[j].site] = x[j].imageCount
       }
-      console.log(siteCounts)
-      replaceChartData(CHARTS.bubbleChart, bubbles, [])
-      replaceChartData(CHARTS.pieChart, Object.values(modeCounts), Object.keys(modeCounts))
-      replaceChartData(CHARTS.barChart, Object.values(siteCounts), Object.keys(siteCounts))
-    })
+      if (modeCounts.hasOwnProperty(x[j].modality)) {
+        modeCounts[x[j].modality] = modeCounts[x[j].modality] + x[j].imageCount
+      } else {
+        modeCounts[x[j].modality] = x[j].imageCount
+      }
+      bubbles.push({
+        x: x[j].subjectCount,
+        y: x[j].studyCount,
+        r: Math.log(x[j].imageCount)
+      })
+    }
+    console.log(siteCounts)
+    replaceChartData(CHARTS.bubbleChart, bubbles, [])
+    replaceChartData(CHARTS.pieChart, Object.values(modeCounts), Object.keys(modeCounts))
+    replaceChartData(CHARTS.barChart, Object.values(siteCounts), Object.keys(siteCounts))
+  })
 }
