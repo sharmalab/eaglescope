9e68fd7a2246971364c7c06b82ebd1402b63c375
Ryan Birmingham <birm@rbirm.us>
2021-12-02 15:05:11 -0500

    dark theme mods from Jeff

--- dist/VisDataTable.b84a7b77.js
+++ dist/VisDataTable.b84a7b77.js
@@ -20488,7 +20488,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35473" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41567" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
