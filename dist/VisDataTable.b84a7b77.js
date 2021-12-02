43ac0dbbd701c3bbf6b5917eae07fa58aa19980f
Ryan Birmingham <birm@rbirm.us>
2021-12-02 15:52:03 -0500

    try to fix configurl

--- dist/VisDataTable.b84a7b77.js
+++ dist/VisDataTable.b84a7b77.js
@@ -20488,7 +20488,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37059" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46493" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
