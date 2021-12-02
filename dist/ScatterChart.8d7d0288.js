b051a93e0e99a978838b10163ad34e1029a58799
Ryan Birmingham <birm@rbirm.us>
2021-12-02 15:19:34 -0500

    force no border in panels and bar

--- dist/ScatterChart.8d7d0288.js
+++ dist/ScatterChart.8d7d0288.js
@@ -389,7 +389,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41567" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40209" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
