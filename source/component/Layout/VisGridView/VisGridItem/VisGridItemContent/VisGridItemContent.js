a576c6bc3cfa8218534b6187705f69b6b3fafdf4
Ryan Birmingham <birm@rbirm.us>
2022-03-23 12:17:35 -0400

    no double include hist

--- source/component/Layout/VisGridView/VisGridItem/VisGridItemContent/VisGridItemContent.js
+++ source/component/Layout/VisGridView/VisGridItem/VisGridItemContent/VisGridItemContent.js
@@ -26,11 +26,6 @@ const VisDataTable = React.lazy(() =>
   import("../../../../VisualTools/VisDataTable/VisDataTable")
 );
 
-const Histogram = React.lazy(() =>
-  import("../../../../VisualTools/Chart/Histogram")
-);
-
-
 const VisGridCard = React.lazy(() =>
   import("../../../../VisualTools/VisGridCard/VisGridCard")
 );
