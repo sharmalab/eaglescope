71958bf522053772318e312851a258ce2b638947
Ryan Birmingham <birm@rbirm.us>
2022-06-10 14:07:03 -0400

    add config

--- dist/HorizontalBarChart.ff0fe6a7.js
+++ dist/HorizontalBarChart.ff0fe6a7.js
@@ -388,7 +388,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46049" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38361" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
