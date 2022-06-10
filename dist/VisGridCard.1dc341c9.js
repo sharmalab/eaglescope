f5ef12a96152b84f50a73c868befbf5886b07d0b
Ryan Birmingham <birm@rbirm.us>
2022-06-10 14:31:06 -0400

    rebuild

--- dist/VisGridCard.1dc341c9.js
+++ dist/VisGridCard.1dc341c9.js
@@ -2044,7 +2044,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38361" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38085" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
