bdc42b9297fcaa4d5f764bdddb6d6f7b1d2d8974
Ryan Birmingham <birm@rbirm.us>
2022-07-28 15:58:45 -0400

    build: btn color changes

--- dist/VisDataTable.b84a7b77.js
+++ dist/VisDataTable.b84a7b77.js
@@ -7294,7 +7294,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35859" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34371" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
