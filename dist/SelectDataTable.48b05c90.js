68e2bcd02950eb44640462825512cf5213ff776b
Birm <birm@rbirm.us>
2024-06-14 23:41:59 -0400

    try cache csvs build

--- dist/SelectDataTable.48b05c90.js
+++ dist/SelectDataTable.48b05c90.js
@@ -569,7 +569,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59584" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59807" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
