adfe295ab92c31779e0b0cb5aa8840828497ee37
Birm <birm@rbirm.us>
2024-06-27 09:57:40 -0400

    build dist

--- dist/BarChart.5f4d3c70.js
+++ dist/BarChart.5f4d3c70.js
@@ -365,7 +365,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52132" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54254" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
