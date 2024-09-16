b273e7df3b18138dd0b0ff29f1d4b7cccd03641c
Birm <birm@rbirm.us>
2024-09-16 12:22:23 -0400

    add google tracker

--- dist/index.js
+++ dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54603" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60560" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
