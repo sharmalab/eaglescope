9e2069b6339c2298f717aa769d54d68b7dc0560a
Birm <birm@rbirm.us>
2024-12-06 11:27:34 -0500

    rebuild dist

--- dist/DensityChart.25818996.js
+++ dist/DensityChart.25818996.js
@@ -291,7 +291,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60405" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63893" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
