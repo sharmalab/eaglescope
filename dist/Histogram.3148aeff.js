71ae60c582d32d9d9e0fa5ebad04f301f32f327b
Birm <birm@rbirm.us>
2025-02-10 12:13:17 -0500

    rebuild dist

--- dist/Histogram.3148aeff.js
+++ dist/Histogram.3148aeff.js
@@ -306,7 +306,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61069" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49703" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- dist/VisDataTable.c1822ccd.js
+++ dist/VisDataTable.c1822ccd.js
@@ -463,7 +463,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61069" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49703" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
