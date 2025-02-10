9e9a8bd0948f79b275a02d1a534e65e1616f86cc
Birm <birm@rbirm.us>
2025-02-10 12:14:32 -0500

    better numeric check

--- source/hooks/useFetch.js
+++ source/hooks/useFetch.js
@@ -2,13 +2,7 @@ import { useEffect, useState } from 'react';
 import * as d3 from 'd3';
 
 function isNumeric(str) {
-  if (typeof str !== 'string') return false; // we only process strings!
-  return (
-    !Number.isNaN(str)
-    // use type coercion to parse the _entirety_ of the string
-    // (`parseFloat` alone does not do this)...
-    && !Number.isNaN(parseFloat(str))
-  ); // ...and ensure strings of whitespace fail
+  return typeof str === 'string' && /^[+-]?(?:\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$/.test(str);
 }
 
 function covertRaw(elt) {
