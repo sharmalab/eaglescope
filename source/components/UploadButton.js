c5c67e97e101c69abcff8e7462d2cc8c2362e9a6
Birm <birm@rbirm.us>
2025-02-13 12:31:43 -0500

    add url, destination name to upload modal

--- source/components/UploadButton.js
+++ source/components/UploadButton.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUpload , faChartBar} from '@fortawesome/free-solid-svg-icons';
+import { faUpload, faChartBar } from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import * as d3 from 'd3';
@@ -9,22 +9,19 @@ class UploadButton extends PureComponent {
   constructor(props, ctx) {
     super(props, ctx);
     this.state = {
-      showModal: false,      // modal visibility
-      file: null,            // selected file
-      fileContent: null,     // file content
+      showModal: false, // modal visibility
+      file: null, // selected file
+      fileContent: null, // file content
       isLoading: false,
+      name: '', // name input field
+      url: '', // URL input field
     };
 
     this.handleFileChange = this.handleFileChange.bind(this);
     this.handleUpload = this.handleUpload.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
     this.handleSampleAnalysis = this.handleSampleAnalysis.bind(this);
-  }
-
-  toggleModal() {
-    this.setState(prevState => ({
-      showModal: !prevState.showModal,
-    }));
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleFileChange(event) {
@@ -32,25 +29,32 @@ class UploadButton extends PureComponent {
     this.setState({ file });
   }
 
-  handleUpload() {
-    const { file } = this.state;
+  handleInputChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
 
-    if (!file) {
-      alert('Please select a CSV file to upload');
+  handleUpload() {
+    const { file, url, name } = this.state;
+
+    if (!file && !url) {
+      alert('Please select a CSV file or provide a URL');
       return;
     }
 
     this.setState({ isLoading: true });
 
-    // Use d3.csv to parse the uploaded file
-    d3.csv(URL.createObjectURL(file))
+    const fileUrl = url || (file && URL.createObjectURL(file));
+
+    // Use d3.csv to parse the uploaded file or the URL
+    d3.csv(fileUrl)
       .then((data) => {
         // Save the file and parsed content in local storage
         localStorage.setItem('uploadedCSV', JSON.stringify(data));
         this.setState({ fileContent: data, isLoading: false });
 
         // Trigger sample analysis
-        this.handleSampleAnalysis(data);
+        this.handleSampleAnalysis(data, name, url);
         this.toggleModal(); // Close the modal after successful upload
       })
       .catch((error) => {
@@ -61,11 +65,22 @@ class UploadButton extends PureComponent {
   }
 
   handleSampleAnalysis(data) {
-    console.error("TODO!!", data);
+    console.log('TODO!!', data, this.state.name);
+    if (this.state.url) {
+      console.log('I actually got a url source', this.state.url);
+    }
+  }
+
+  toggleModal() {
+    this.setState((prevState) => ({
+      showModal: !prevState.showModal,
+    }));
   }
 
   render() {
-    const { showModal, isLoading } = this.state;
+    const {
+      showModal, file, isLoading, name, url,
+    } = this.state;
 
     return (
       <div>
@@ -73,12 +88,12 @@ class UploadButton extends PureComponent {
           size="lg"
           style={{
             background: 'none',
-            border: '2px solid #ccc',  // Adding a subtle border
-            borderRadius: '12px',      // Rounded corners
-            position: 'relative',      // Position relative for absolute positioning of icons
-            width: '50px',             // Set width to fit icon size
-            height: '50px',            // Set height to fit icon size
-            padding: 0,                // Remove padding
+            border: '2px solid #ccc', // Adding a subtle border
+            borderRadius: '12px', // Rounded corners
+            position: 'relative', // Position relative for absolute positioning of icons
+            width: '50px', // Set width to fit icon size
+            height: '50px', // Set height to fit icon size
+            padding: 0, // Remove padding
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
@@ -121,10 +136,34 @@ class UploadButton extends PureComponent {
                   accept=".csv"
                   onChange={this.handleFileChange}
                 />
+                <div>
+                  <label>
+                    Name (optional):
+                    <input
+                      type="text"
+                      name="name"
+                      value={name}
+                      onChange={this.handleInputChange}
+                      placeholder="local dashboard name"
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    URL (optional):
+                    <input
+                      type="text"
+                      name="url"
+                      value={url}
+                      onChange={this.handleInputChange}
+                      placeholder="Enter CSV file URL"
+                    />
+                  </label>
+                </div>
                 <Button
                   variant="primary"
                   onClick={this.handleUpload}
-                  disabled={!this.state.file}
+                  disabled={!file && !url}
                 >
                   Upload
                 </Button>
